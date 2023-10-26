4a8d3502760092a535ebe0bee2976e417faa5a59
unknown <danleviobello@gmail.com>
2023-10-25 22:01:22 -0500

    Says I had changes, but I dont know what lol

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import Header from './components/Header'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 
-function App() {
+export default function App() {
     return (
       <div className="App">
         <Header />
         <Nav />
-        <Outlet />
         <Footer />
       </div>
     );
   
   }
-  
-  export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App from './App';
+import App from './App'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
-import './index.css';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
-            { path: '', element: <About /> },
+            { index: true, element: <About /> },
             { path: 'about', element: <About /> },
             { path: 'projects', element: <Projects /> },
             { path: 'contact', element: <Contact /> }
